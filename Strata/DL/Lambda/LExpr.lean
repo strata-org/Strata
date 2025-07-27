618e4fd37a573ce50192af08bf076f30df11e72d
Shilpi Goel <shilgoel@amazon.com>
2025-07-27 15:17:52 -0500

    Move the GettingStarted guide in docs/

--- Strata/DL/Lambda/LExpr.lean
+++ Strata/DL/Lambda/LExpr.lean
@@ -30,16 +30,12 @@ Formalization of Lambda Expressions
 namespace Lambda
 open Std (ToFormat Format format)
 
-/-! # Lambda Dialect
-
-This dialect provides an implementation of simply-typed lambda calculus, along
-with a Hindley-Milner type system. It also comes with an extensible partial
-evaluator that is parameterized by an (optional) map from operator names to
-their specific evaluations. This allows adding evaluation support for new
-operators without changing the core logic or extending the AST.
--/
-
 /-! ## Lambda Expressions with Quantifiers -/
+
+inductive QuantifierKind
+  | all
+  | exist
+  deriving Repr, DecidableEq
 
 /--
 Lambda Expressions with Quantifiers.
@@ -53,11 +49,6 @@ We leave placeholders for (mono)type annotations only for constants
 variables (`.fvar`). For a fully (mono)type annotated AST, see `LExprT`
 that is created after the type inference transform.
 -/
-inductive QuantifierKind
-  | all
-  | exist
-  deriving Repr, DecidableEq
-
 inductive LExpr (Identifier : Type) : Type where
   | const   (c : String) (ty : Option LMonoTy)
   | op      (o : Identifier) (ty : Option LMonoTy)
