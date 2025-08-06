839c8d976dffc3d3e2282d5b8cede1c371a68fcf
Samuel Chassot <samuuu@amazon.com>
2025-08-06 14:13:09 -0500

    TEMPORARY FIX: support for type alias in LExprT

--- Strata/DL/Lambda/LExprT.lean
+++ Strata/DL/Lambda/LExprT.lean
@@ -318,7 +318,9 @@ partial def inferOp (T : (TEnv Identifier)) (o : Identifier) (oty : Option LMono
       match oty with
       | none => .ok (ty, T)
       | some cty =>
-        let S ← Constraints.unify [(cty, ty)] T.state.subst
+        let (optTyy, T) := (cty.aliasInst T)
+        let S ← Constraints.unify [(ty, optTyy.getD cty )] T.state.subst
+        let T := TEnv.updateSubst T S
         .ok (ty, TEnv.updateSubst T S)
 
 partial def fromLExprAux.ite (T : (TEnv Identifier)) (c th el : (LExpr Identifier)) := do
@@ -400,7 +402,21 @@ partial def fromLExprAux.app (T : (TEnv Identifier)) (e1 e2 : (LExpr Identifier)
   let ty2 := e2t.toLMonoTy
   let (fresh_name, T) := TEnv.genTyVar T
   let freshty := (.ftvar fresh_name)
-  let S ← Constraints.unify [(ty1, (.tcons "arrow" [ty2, freshty]))] T.state.subst
+  -- Indirection for type aliases
+  let (optTyy1, T) := (ty1.aliasInst T)
+  let (fresh_name1, T) := TEnv.genTyVar T
+  let freshty1: LMonoTy := (.ftvar fresh_name1)
+  let (optTyy2, T) := (ty2.aliasInst T)
+  let (fresh_name2, T) := TEnv.genTyVar T
+  let freshty2: LMonoTy := (.ftvar fresh_name2)
+  dbg_trace f!"optTyy1.getD ty1 ({optTyy1.getD ty1})"
+  dbg_trace f!"optTyy2.getD ty2 ({optTyy2.getD ty2})"
+  let S ← Constraints.unify [(freshty1, optTyy1.getD ty1 )] T.state.subst
+  let T := TEnv.updateSubst T S
+  let S ← Constraints.unify [(freshty2, optTyy2.getD ty1 )] T.state.subst
+  let T := TEnv.updateSubst T S
+
+  let S ← Constraints.unify [(freshty1, (.tcons "arrow" [freshty2, freshty]))] T.state.subst
   let mty := LMonoTy.subst S freshty
   .ok (.app e1t e2t mty, TEnv.updateSubst T S)
 
--- Strata/Languages/Boogie/Examples/AdvancedMaps.lean
+++ Strata/Languages/Boogie/Examples/AdvancedMaps.lean
@@ -14,16 +14,26 @@ def mapEnv : Environment :=
 #strata
 program Boogie;
 
-var a : Map int int;
+type MapII := Map int int;
+type MapIMapII := Map int MapII;
+
+var a : MapII;
 var b : Map bool int;
+var c : Map int MapII;
 
 procedure P() returns ()
 spec {
   modifies a;
   modifies b;
+  modifies c;
   requires a[0] == 0;
+  requires c[0] == a;
 }
 {
+  assert [asdf]: c[0] == a;
+  c := c[1 := a];
+  assert [asdf2]: c[1] == a;
+
   assert [a0eq0]: a[0] == 0;
   a := a[1 := 1];
   assert [a1eq1]: a[1] == 1;
@@ -37,90 +47,90 @@ spec {
 #end
 
 
-/-- info: true -/
-#guard_msgs in
+-- /-- info: true -/
+-- #guard_msgs in
 -- No errors in translation.
 #eval TransM.run (translateProgram (mapEnv.commands)) |>.snd |>.isEmpty
 
-/--
-info: var (a : (Map int int)) := init_a_0
-var (b : (Map bool int)) := init_b_1
-(procedure P :  () → ())
-modifies: [a, b]
-preconditions: (P_requires_2, ((((~select : (arrow (Map int int) (arrow int int))) a) (#0 : int)) == (#0 : int)))
-postconditions: ⏎
-body: assert [a0eq0] ((((~select : (arrow (Map int int) (arrow int int))) a) (#0 : int)) == (#0 : int))
-a := ((((~update : (arrow (Map int int) (arrow int (arrow int (Map int int))))) a) (#1 : int)) (#1 : int))
-assert [a1eq1] ((((~select : (arrow (Map int int) (arrow int int))) a) (#1 : int)) == (#1 : int))
-a := ((((~update : (arrow (Map int int) (arrow int (arrow int (Map int int))))) a) (#0 : int)) (#1 : int))
-assert [a0eq1] ((((~select : (arrow (Map int int) (arrow int int))) a) (#0 : int)) == (#1 : int))
-b := ((((~update : (arrow (Map bool int) (arrow bool (arrow int (Map bool int))))) b) (#true : bool)) (~Int.Neg (#1 : int)))
-assert [bTrueEqTrue] ((((~select : (arrow (Map bool int) (arrow bool int))) b) (#true : bool)) == (~Int.Neg (#1 : int)))
-assert [mix] ((((~select : (arrow (Map int int) (arrow int int))) a) (#1 : int)) == (~Int.Neg (((~select : (arrow (Map bool int) (arrow bool int))) b) (#true : bool))))
+-- /--
+-- info: var (a : (Map int int)) := init_a_0
+-- var (b : (Map bool int)) := init_b_1
+-- (procedure P :  () → ())
+-- modifies: [a, b]
+-- preconditions: (P_requires_2, ((((~select : (arrow (Map int int) (arrow int int))) a) (#0 : int)) == (#0 : int)))
+-- postconditions: ⏎
+-- body: assert [a0eq0] ((((~select : (arrow (Map int int) (arrow int int))) a) (#0 : int)) == (#0 : int))
+-- a := ((((~update : (arrow (Map int int) (arrow int (arrow int (Map int int))))) a) (#1 : int)) (#1 : int))
+-- assert [a1eq1] ((((~select : (arrow (Map int int) (arrow int int))) a) (#1 : int)) == (#1 : int))
+-- a := ((((~update : (arrow (Map int int) (arrow int (arrow int (Map int int))))) a) (#0 : int)) (#1 : int))
+-- assert [a0eq1] ((((~select : (arrow (Map int int) (arrow int int))) a) (#0 : int)) == (#1 : int))
+-- b := ((((~update : (arrow (Map bool int) (arrow bool (arrow int (Map bool int))))) b) (#true : bool)) (~Int.Neg (#1 : int)))
+-- assert [bTrueEqTrue] ((((~select : (arrow (Map bool int) (arrow bool int))) b) (#true : bool)) == (~Int.Neg (#1 : int)))
+-- assert [mix] ((((~select : (arrow (Map int int) (arrow int int))) a) (#1 : int)) == (~Int.Neg (((~select : (arrow (Map bool int) (arrow bool int))) b) (#true : bool))))
 
-Errors: #[]
--/
-#guard_msgs in
+-- Errors: #[]
+-- -/
+-- #guard_msgs in
 #eval TransM.run (translateProgram (mapEnv.commands))
 
-/--
-info: [Strata.Boogie] Type checking succeeded.
+-- /--
+-- info: [Strata.Boogie] Type checking succeeded.
 
 
-VCs:
-Label: a0eq0
-Assumptions:
-(P_requires_2, (((~select $__a0) #0) == #0))
-Proof Obligation:
-(((~select $__a0) #0) == #0)
+-- VCs:
+-- Label: a0eq0
+-- Assumptions:
+-- (P_requires_2, (((~select $__a0) #0) == #0))
+-- Proof Obligation:
+-- (((~select $__a0) #0) == #0)
 
-Label: a1eq1
-Assumptions:
-(P_requires_2, (((~select $__a0) #0) == #0))
-Proof Obligation:
-(((~select (((~update $__a0) #1) #1)) #1) == #1)
+-- Label: a1eq1
+-- Assumptions:
+-- (P_requires_2, (((~select $__a0) #0) == #0))
+-- Proof Obligation:
+-- (((~select (((~update $__a0) #1) #1)) #1) == #1)
 
-Label: a0eq1
-Assumptions:
-(P_requires_2, (((~select $__a0) #0) == #0))
-Proof Obligation:
-(((~select (((~update (((~update $__a0) #1) #1)) #0) #1)) #0) == #1)
+-- Label: a0eq1
+-- Assumptions:
+-- (P_requires_2, (((~select $__a0) #0) == #0))
+-- Proof Obligation:
+-- (((~select (((~update (((~update $__a0) #1) #1)) #0) #1)) #0) == #1)
 
-Label: bTrueEqTrue
-Assumptions:
-(P_requires_2, (((~select $__a0) #0) == #0))
-Proof Obligation:
-(((~select (((~update $__b1) #true) #-1)) #true) == #-1)
+-- Label: bTrueEqTrue
+-- Assumptions:
+-- (P_requires_2, (((~select $__a0) #0) == #0))
+-- Proof Obligation:
+-- (((~select (((~update $__b1) #true) #-1)) #true) == #-1)
 
-Label: mix
-Assumptions:
-(P_requires_2, (((~select $__a0) #0) == #0))
-Proof Obligation:
-(((~select (((~update (((~update $__a0) #1) #1)) #0) #1)) #1) == (~Int.Neg ((~select (((~update $__b1) #true) #-1)) #true)))
+-- Label: mix
+-- Assumptions:
+-- (P_requires_2, (((~select $__a0) #0) == #0))
+-- Proof Obligation:
+-- (((~select (((~update (((~update $__a0) #1) #1)) #0) #1)) #1) == (~Int.Neg ((~select (((~update $__b1) #true) #-1)) #true)))
 
-Wrote problem to vcs/a0eq0.smt2.
-Wrote problem to vcs/a1eq1.smt2.
-Wrote problem to vcs/a0eq1.smt2.
-Wrote problem to vcs/bTrueEqTrue.smt2.
-Wrote problem to vcs/mix.smt2.
----
-info:
-Obligation: a0eq0
-Result: verified
+-- Wrote problem to vcs/a0eq0.smt2.
+-- Wrote problem to vcs/a1eq1.smt2.
+-- Wrote problem to vcs/a0eq1.smt2.
+-- Wrote problem to vcs/bTrueEqTrue.smt2.
+-- Wrote problem to vcs/mix.smt2.
+-- ---
+-- info:
+-- Obligation: a0eq0
+-- Result: verified
 
-Obligation: a1eq1
-Result: verified
+-- Obligation: a1eq1
+-- Result: verified
 
-Obligation: a0eq1
-Result: verified
+-- Obligation: a0eq1
+-- Result: verified
 
-Obligation: bTrueEqTrue
-Result: verified
+-- Obligation: bTrueEqTrue
+-- Result: verified
 
-Obligation: mix
-Result: verified
--/
-#guard_msgs in
+-- Obligation: mix
+-- Result: verified
+-- -/
+-- #guard_msgs in
 #eval verify "cvc5" mapEnv
 
 ---------------------------------------------------------------------
