0f8ea4fc57fe313355e9b77abc5b17e5093d949b
Andrew Wells <andrewmw94@gmail.com>
2025-08-25 16:20:28 -0500

    Unit tests for Strata.Dyn

--- Strata/Languages/Dyn/Examples/Trivial.lean
+++ Strata/Languages/Dyn/Examples/Trivial.lean
@@ -21,5 +21,41 @@ def trivial (x : Any) -> Any
 
 #end
 
+/--
+info: #[{ name := { dialect := "Dyn", name := "function_def" },
+    args :=
+      ((((Array.mkEmpty 4).push (Arg.type (TypeExpr.ident { dialect := "Dyn", name := "Any" } (Array.mkEmpty 0)))).push
+                (Arg.ident "trivial")).push
+            (Arg.op
+              { name := { dialect := "Dyn", name := "mkParams" },
+                args :=
+                  (Array.mkEmpty 1).push
+                    (Arg.commaSepList
+                      ((Array.mkEmpty 1).push
+                        (Arg.op
+                          { name := { dialect := "Dyn", name := "mkParam" },
+                            args :=
+                              ((Array.mkEmpty 2).push (Arg.ident "x")).push
+                                (Arg.type
+                                  (TypeExpr.ident { dialect := "Dyn", name := "Any" } (Array.mkEmpty 0))) }))) })).push
+        (Arg.op
+          { name := { dialect := "Dyn", name := "block" },
+            args :=
+              (Array.mkEmpty 1).push
+                (Arg.seq
+                  ((Array.mkEmpty 1).push
+                    (Arg.op
+                      { name := { dialect := "Dyn", name := "return_stmt" },
+                        args :=
+                          ((Array.mkEmpty 2).push
+                                (Arg.type (TypeExpr.ident { dialect := "Dyn", name := "Any" } (Array.mkEmpty 0)))).push
+                            (Arg.expr
+                              ((Expr.fn { dialect := "Dyn", name := "int_to_Any" }).app
+                                (Arg.expr
+                                  ((Expr.fn { dialect := "Dyn", name := "to_int" }).app (Arg.num 0))))) }))) }) }]
+-/
+#guard_msgs in
+#eval TrivialPgm.commands
+
 end Dyn
 end Strata
