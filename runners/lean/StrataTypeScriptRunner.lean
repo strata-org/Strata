b0d4488b718c6d88e8cc329c2fb96fd000c231bd
Andrew Wells <anmwells@amazon.com>
2025-10-07 15:25:39 -0500

    Fix copyright headers

--- runners/lean/StrataTypeScriptRunner.lean
+++ runners/lean/StrataTypeScriptRunner.lean
@@ -1,3 +1,9 @@
+/-
+  Copyright Strata Contributors
+
+  SPDX-License-Identifier: Apache-2.0 OR MIT
+-/
+
 import Strata.Languages.TypeScript.js_ast
 import Strata.Languages.TypeScript.TSStrataEval
 
