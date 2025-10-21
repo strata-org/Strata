8525b14b1454b64c30447ce04b987b69efb609b7
Andrew Wells <anmwells@amazon.com>
2025-10-21 14:58:15 -0500

    Add Internal folder with empty BoogiePrelude

--- Strata/Languages/Python/Verify.lean
+++ Strata/Languages/Python/Verify.lean
@@ -1,8 +1,13 @@
 import Strata.DDM.Elab
 import Strata.DDM.AST
 import Strata.Languages.Python.PythonToBoogie
+import Strata.Languages.Boogie.DDMTransform.Parse
+import StrataTest.Internal.BoogiePrelude
 
 namespace Strata
+
+private def f : String := boogiePrelude
+
 
 def pythonVerify (pgm: Strata.Program) : String :=
   let boogiePgm :=pythonToBoogie pgm
---
+++ StrataTest/Internal/BoogiePrelude.lean
@@ -0,0 +1,10 @@
+import Strata.DDM.Elab
+import Strata.DDM.AST
+import Strata.Languages.Python.PythonToBoogie
+import Strata.Languages.Boogie.DDMTransform.Parse
+
+namespace Strata
+
+def boogiePrelude: String := "Foo"
+
+end Strata
