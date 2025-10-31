586ab91e01431bff06051f65c5d15b25f5a635b7
Shilpi Goel <shilgoel@amazon.com>
2025-10-31 13:47:27 -0500

    Add support for SMT encoding of Re.Loop

--- Strata/Languages/Boogie/Examples/Regex.lean
+++ Strata/Languages/Boogie/Examples/Regex.lean
@@ -17,53 +17,112 @@ function cannot_end_with_period () : regex {
   re.comp(re.concat (re.* (re.all()), str.to.re(".")))
 }
 
+function ok_chars_regex () : regex {
+    re.loop(
+        re.union(re.range("a", "z"),
+            re.union(re.range("0", "9"),
+                     re.union(str.to.re("."),
+                              str.to.re("-")))),
+        1, 10)
+}
+
 procedure main() returns () {
 
-    assert (!(str.in.re("hello.", cannot_end_with_period())));
-    assert (!(str.in.re(".",      cannot_end_with_period())));
-    assert   (str.in.re("bye!",   cannot_end_with_period()));
+    assert [hello_dot_ends_with_period]:    (!(str.in.re("hello.", cannot_end_with_period())));
+    assert [dot_ends_with_period]:          (!(str.in.re(".",      cannot_end_with_period())));
+    assert [bye_exclaim_no_end_with_period]:  (str.in.re("bye!",   cannot_end_with_period()));
+
+    assert [ok_chars_str]:                    (str.in.re("test-str-1", ok_chars_regex()));
+    assert [cannot_contain_exclaim]:        (!(str.in.re("test-str!", ok_chars_regex())));
+    assert [has_to_be_at_least_1_char]:     (!(str.in.re("", ok_chars_regex())));
+    assert [cannot_exceed_10_chars]:        (!(str.in.re("0123456789a", ok_chars_regex())));
 
 };
 #end
+
 
 /--
 info: [Strata.Boogie] Type checking succeeded.
 
 
 VCs:
-Label: assert_0
+Label: hello_dot_ends_with_period
 Assumptions:
 
 
 Proof Obligation:
 (~Bool.Not ((~Str.InRegEx #hello.) ~cannot_end_with_period))
 
-Label: assert_1
+Label: dot_ends_with_period
 Assumptions:
 
 
 Proof Obligation:
 (~Bool.Not ((~Str.InRegEx #.) ~cannot_end_with_period))
 
-Label: assert_2
+Label: bye_exclaim_no_end_with_period
 Assumptions:
 
 
 Proof Obligation:
 ((~Str.InRegEx #bye!) ~cannot_end_with_period)
 
-Wrote problem to vcs/assert_0.smt2.
-Wrote problem to vcs/assert_1.smt2.
-Wrote problem to vcs/assert_2.smt2.
+Label: ok_chars_str
+Assumptions:
+
+
+Proof Obligation:
+((~Str.InRegEx #test-str-1) ~ok_chars_regex)
+
+Label: cannot_contain_exclaim
+Assumptions:
+
+
+Proof Obligation:
+(~Bool.Not ((~Str.InRegEx #test-str!) ~ok_chars_regex))
+
+Label: has_to_be_at_least_1_char
+Assumptions:
+
+
+Proof Obligation:
+(~Bool.Not ((~Str.InRegEx #) ~ok_chars_regex))
+
+Label: cannot_exceed_10_chars
+Assumptions:
+
+
+Proof Obligation:
+(~Bool.Not ((~Str.InRegEx #0123456789a) ~ok_chars_regex))
+
+Wrote problem to vcs/hello_dot_ends_with_period.smt2.
+Wrote problem to vcs/dot_ends_with_period.smt2.
+Wrote problem to vcs/bye_exclaim_no_end_with_period.smt2.
+Wrote problem to vcs/ok_chars_str.smt2.
+Wrote problem to vcs/cannot_contain_exclaim.smt2.
+Wrote problem to vcs/has_to_be_at_least_1_char.smt2.
+Wrote problem to vcs/cannot_exceed_10_chars.smt2.
 ---
 info:
-Obligation: assert_0
+Obligation: hello_dot_ends_with_period
 Result: verified
 
-Obligation: assert_1
+Obligation: dot_ends_with_period
 Result: verified
 
-Obligation: assert_2
+Obligation: bye_exclaim_no_end_with_period
+Result: verified
+
+Obligation: ok_chars_str
+Result: verified
+
+Obligation: cannot_contain_exclaim
+Result: verified
+
+Obligation: has_to_be_at_least_1_char
+Result: verified
+
+Obligation: cannot_exceed_10_chars
 Result: verified
 -/
 #guard_msgs in
