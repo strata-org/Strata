9c8d1ba38bc81a0f89525ef4dde61e64bb3ca388
Aaron Tomb <aarotomb@amazon.com>
2025-11-05 13:49:47 -0800

    Set up langdef doc to import the Strata package
    This allows us to write definitions in the doc that use Strata types.

--- docs/verso/LangDefDoc.lean
+++ docs/verso/LangDefDoc.lean
@@ -6,6 +6,14 @@
 
 import VersoManual
 
+import Strata.DL.Imperative.Cmd
+import Strata.DL.Imperative.CmdSemantics
+import Strata.DL.Imperative.Stmt
+import Strata.DL.Imperative.StmtSemantics
+import Strata.DL.Lambda.LExpr
+open Imperative
+open Lambda
+
 -- This gets access to most of the manual genre
 open Verso.Genre Manual
 
@@ -21,4 +29,35 @@ authors := ["Aaron Tomb", "TODO"]
 shortTitle := "Strata Language"
 %%%
 
-TODO
+TODO: below is an example of what including Lean code defining the semantics of
+Strata might look like.
+
+```lean
+namespace Imperative
+
+```
+# Small-Step Operational Semantics for Statements
+
+Here we define small-step operational semantics for the
+`Imperative` dialect's statement constructs.
+
+First, we define the notion of a configuration, representing the
+current execution state. A configuration consists of:
+- The current statement or statements being executed
+- The current store (for current variable values)
+- The initial store (for evaluating Boogie's `old` operator)
+
+```lean
+inductive Config (P : PureExpr) : Type where
+  | stmt :
+      Stmt P (Cmd P) →
+      SemanticStore P →
+      SemanticStore P →
+      Config P
+  | stmts :
+      List (Stmt P (Cmd P)) →
+      SemanticStore P →
+      SemanticStore P →
+      Config P
+  | terminal : SemanticStore P → SemanticStore P → Config P
+```
