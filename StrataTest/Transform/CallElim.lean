c09848c738fbb45c1fc340e95ebd504bc9b186a4
Juneyoung Lee <lebjuney@amazon.com>
2025-11-17 11:28:29 -0600

    Move Strata/Transform/Examples.lean to StrataTest/Transform/*

--- StrataTest/Transform/CallElim.lean
+++ StrataTest/Transform/CallElim.lean
@@ -5,13 +5,10 @@
 -/
 
 import Strata.Transform.CallElim
-import Strata.Transform.DetToNondet
 import Strata.Languages.Boogie.StatementSemantics
 import Strata.Languages.Boogie.ProgramType
 import Strata.Languages.Boogie.ProgramWF
 import Strata.DL.Lambda.IntBoolFactory
-
-/-! # Program Transformation Examples -/
 
 open Boogie
 open CallElim
@@ -206,30 +203,3 @@ info: true
 --#eval tests[1].snd
 
 end CallElimExamples
-
-/-! ## Deterministic-to-Nondeterministic Examples -/
-section NondetExamples
-
-open Imperative
-
-def NondetTest1 : Stmt Expression (Cmd Expression) :=
-  .ite (Boogie.true) {ss :=
-    [.cmd $ .havoc "x" ]
-    } {ss :=
-    [.cmd $ .havoc "y" ]
-    }
-
-def NondetTest1Ans : NondetStmt Expression (Cmd Expression) :=
-  .choice
-    (.seq (.cmd (.assume "true_cond" Boogie.true)) (.seq (.cmd $ .havoc "x") (.assume "skip" Imperative.HasBool.tt)))
-    (.seq (.cmd (.assume "false_cond" Boogie.false)) (.seq (.cmd $ .havoc "y") (.assume "skip" Imperative.HasBool.tt)))
-
-
--- #eval toString $ Std.format (StmtToNondetStmt NondetTest1)
--- #eval toString $ Std.format NondetTest1Ans
-
-/-- info: true -/
-#guard_msgs in
-#eval (toString $ Std.format (StmtToNondetStmt NondetTest1)) == (toString $ Std.format NondetTest1Ans)
-
-end NondetExamples
