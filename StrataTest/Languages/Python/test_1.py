b49f2978b6bbf6622dd3f4e7377ec93d888a5b6a
Andrew Wells <anmwells@amazon.com>
2025-11-19 11:27:19 -0600

    Fix test_1

--- StrataTest/Languages/Python/test_1.py
+++ StrataTest/Languages/Python/test_1.py
@@ -1,13 +1,12 @@
 import test_helper
 
-# Test function inlining
+# Test function defs
 
 def my_f(s: str) -> None:
     test_helper.procedure(s)
 
 def main():
     my_f("foo")
-    my_f("Foo")
 
 if __name__ == "__main__":
     main()
