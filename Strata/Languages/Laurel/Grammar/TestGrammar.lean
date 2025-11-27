757af8e3c44eeee6638609aec8059388c3a5d4ec
Remy Willems <rwillems@amazon.com>
2025-11-27 16:33:49 +0100

    Change TestGrammar

--- Strata/Languages/Laurel/Grammar/TestGrammar.lean
+++ Strata/Languages/Laurel/Grammar/TestGrammar.lean
@@ -1,22 +1,10 @@
 -- Test the minimal Laurel grammar
 import Strata.Languages.Laurel.Grammar.LaurelGrammar
 
--- Test parsing the AssertFalse example
-def assertFalseTest := #strata
-program LaurelMinimal;
+-- Test parsing the AssertFalse example by reading from file
+def assertFalseTest : IO Unit := do
+  let content ← IO.FS.readFile "Strata/Languages/Laurel/Examples/Fundamentals/AssertFalse.lr.st"
+  IO.println content
 
-procedure foo() {
-  assert true;
-  assert false;
-  assert false;
-}
-
-procedure bar() {
-  assume false;
-  assert true;
-}
-
-#end
-
-#eval IO.println assertFalseTest
+#eval assertFalseTest
 #eval IO.println "Minimal Laurel grammar test completed"
