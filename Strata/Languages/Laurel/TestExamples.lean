fb73018ebc248257427998d3dd71f384a926aa8b
Remy Willems <rwillems@amazon.com>
2025-12-01 16:54:37 +0100

    Add translation to Boogie

--- Strata/Languages/Laurel/TestExamples.lean
+++ Strata/Languages/Laurel/TestExamples.lean
@@ -22,6 +22,103 @@ def testAssertFalse : IO Unit := do
     let results <- verify "cvc5" prog
     IO.println s!"{results}"
 
+/--
+info: Parse succeeded
+[Strata.Boogie] Type checking succeeded.
+
+
+VCs:
+Label: assert
+Assumptions:
+
+
+Proof Obligation:
+#true
+
+Label: assert
+Assumptions:
+
+
+Proof Obligation:
+#false
+
+Label: assert
+Assumptions:
+
+
+Proof Obligation:
+#false
+
+Label: assert
+Assumptions:
+(assume, #false)
+
+Proof Obligation:
+#true
+
+Wrote problem to vcs/assert.smt2.
+
+
+Obligation assert: could not be proved!
+
+Result: failed
+CEx: ⏎
+
+Evaluated program:
+(procedure foo :  () → ())
+modifies: []
+preconditions: ⏎
+postconditions: ⏎
+body: assert [assert] #true
+assert [assert] #false
+assert [assert] #false
+
+(procedure bar :  () → ())
+modifies: []
+preconditions: ⏎
+postconditions: ⏎
+body: assume [assume] #false
+assert [assert] #true
+
+Wrote problem to vcs/assert.smt2.
+
+
+Obligation assert: could not be proved!
+
+Result: failed
+CEx: ⏎
+
+Evaluated program:
+(procedure foo :  () → ())
+modifies: []
+preconditions: ⏎
+postconditions: ⏎
+body: assert [assert] #true
+assert [assert] #false
+assert [assert] #false
+
+(procedure bar :  () → ())
+modifies: []
+preconditions: ⏎
+postconditions: ⏎
+body: assume [assume] #false
+assert [assert] #true
+
+
+Obligation: assert
+Result: verified
+
+Obligation: assert
+Result: failed
+CEx: ⏎
+
+Obligation: assert
+Result: failed
+CEx: ⏎
+
+Obligation: assert
+Result: verified
+-/
 #guard_msgs in
 #eval! testAssertFalse
 
