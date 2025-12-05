d93ab0e7c851c191c83b2a3fb49ffb155585ae58
Aaron Tomb <aarotomb@amazon.com>
2025-12-05 14:32:59 -0800

    Edits to language definition doc

--- docs/verso/LangDefDoc.lean
+++ docs/verso/LangDefDoc.lean
@@ -46,41 +46,41 @@ provides a way to define the syntax and a simple type system for what we call a
 dialect of Strata. At the moment, these dialects do not directly have semantics
 (though we may add a mechanism for defining their semantics in the future) but
 instead are defined by translation to or from the second notion of language,
-Strata’s Core languages. Said another way, each of these dialects is a different
+Strata’s Core language. Said another way, each of these dialects is a different
 concrete way to write Strata programs, but all of these dialects are ultimately
-represented internally using the same Core languages.
+represented internally using the same Core language.
 
 Dialects are used to describe both the initial artifacts being analyzed by
 Strata and more low-level representations of those artifacts used to communicate
-with external reasoning tools. In both situations, Stata uses dialects as a
-mechanism for communicating with external tools (either language front ends or
-generic automated reasoning tools like SMT solvers).
+with external reasoning tools such as model checkers or SMT solvers. In both
+situations, Stata uses dialects as a mechanism for communicating with external
+tools (either language front ends or generic automated reasoning tools like SMT
+solvers).
 
 (TODO: funnel diagram showing various dialects in relation to Strata Core)
 
-The Strata Core languages consist of a few building blocks that can be combined
+The Strata Core language consists of a few building blocks that can be combined
 in different ways. This allows concrete dialects to systematically use different
 combinations that still share the majority of their implementation. In Lean (and
 in principle in most other source languages that could be used to process Strata
 programs), the type system can enforce various structural constraints, ensuring
 that only expected language constructs show up. The most common use of the
-Strata Core languages is in combination: with an imperative statement type
-parameterized by an expression type, and with various more fine-grained
-adjustments of other parameters. Because these building blocks are typically
-used together, it’s reasonable to think of Strata Core as a single language that
-can be configured in various ways for different use cases.
+Strata Core language is with an imperative statement type parameterized by an
+expression type, and with various more fine-grained adjustments of other
+parameters. Because these building blocks are typically used together, it’s
+reasonable to think of Strata Core as a single language that can be configured
+in various ways for different use cases, even though it consists of multiple
+independent types in the Lean implementation.
 
 The two fundamental building blocks of Strata Core are a representation of
 functional programs (`Lambda`), and a representation of imperative programs
-(`Imperative`). The `Lambda` language is parameterized by a type system (TODO:
-and can be constrained to allow or disallow complex features such as lambda
-abstractions and quantifiers). `Lambda` is also parameterized by a set of
-built-in types and functions. The `Imperative` language is then parameterized by
-the type of expressions it allows in conditions, assignments, and so on.
-Currently, those expressions will almost always be some instantiation of
-`Lambda`. Both Core building blocks are parameterized by a metadata type,
-which by default is instantiated with a map from keys to structured values that
-can contain expressions (typically from `Lambda`).
+(`Imperative`). The `Lambda` language is parameterized by a type system and a
+set of built-in types and functions. The `Imperative` language is then
+parameterized by the type of expressions it allows in conditions, assignments,
+and so on. Currently, those expressions will almost always be some
+instantiation of `Lambda`. Both Core building blocks are parameterized by a
+metadata type, which by default is instantiated with a map from keys to
+structured values that can contain expressions (typically from `Lambda`).
 
 The remainder of this document describes the current abstract syntax and
 semantics of `Lambda` and `Imperative` in detail, with direct reference to the
@@ -98,7 +98,7 @@ applications. In addition, it includes a special type of constant, an operator,
 to represent built-in functions. It extends the standard lambda calculus by
 allowing quantifiers (since a key use of the language is to write logical
 predicates) and includes constructors for if-then-else and equality functions,
-since those are so commonly used.
+since those are so commonly used. (TODO: we might remove these last two)
 
 Although `Lambda` can be parameterized by an arbitrary type system, the Strata
 code base includes an implementation of a polymorphic Hindley-Milner type system
@@ -108,16 +108,34 @@ named type constructors (as well as special support for bit vector types, to
 allow them to be parameterized by size).
 
 Type inference and checking with this type system takes as input expressions
-parameterized by `Unit` as the type of types (TODO: is this true?), and produces
-expressions parameterized by `LTy` as output.
+parameterized by {name Unit}`Unit` as the type of types (TODO: is this true?),
+and produces expressions parameterized by {name LTy}`LTy` as output.
 
 ## Syntax
 
-The syntax of lambda expressions is provided by the `LExpr` type.
+The syntax of lambda expressions is provided by the {name LExpr}`LExpr` type.
 
 {docstring Lambda.LConst}
 
 {docstring Lambda.LExpr}
+
+Identifiers in lambda expressions, using the {name Identifier}`Identifier` type,
+can be annotated with metadata.
+
+{docstring Lambda.Identifier}
+
+Specific constructors exist for constants of various scalar types, including
+booleans, bit vectors, integers, reals, and strings.
+
+{docstring Lambda.LConst}
+
+The {name LExpr}`LExpr` type can be parameterized by the type used to represent
+normal metadata and the type used to represent identifier metadata, as well as
+the type of types.
+
+{docstring Lambda.LExprParams}
+
+{docstring Lambda.LExprParamsT}
 
 ## Type System
 
@@ -125,25 +143,44 @@ Although {name LExpr}`LExpr` can be parameterized by an arbitrary type system,
 Strata currently implements one, based on the types {name LMonoTy}`LMonoTy` and
 {name LTy}`LTy`.
 
-The first, {name LMonoTy}`LMonoTy`, represents monomorphic types. It's a separate type because
-some contexts allow only monomorphic types.
+The first, {name LMonoTy}`LMonoTy`, represents monomorphic types. It's a
+separate type because some contexts allow only monomorphic types.
 
 {docstring Lambda.LMonoTy}
 
+Type variables in {name LMonoTy}`LMonoTy` use the {name TyIdentifier}`TyIdentifier` type.
+
+{docstring Lambda.TyIdentifier}
+
 The {name LTy}`LTy` type allows monomorphic types to be wrapped in universal type
-quantifiers, creating polymorphic types.
+quantifiers that bind these type variables, creating polymorphic types.
 
 {docstring Lambda.LTy}
 
-A expression {name LExpr}`LExpr` parameterized by {name LTy}`LTy` is well-typed
-according to the
-{name LExpr.HasType}`HasType` relation.
+A expression {name LExpr}`LExpr` parameterized by {name LTy}`LTy` is
+well-typed according to the {name LExpr.HasType}`HasType` relation.
+This relation depends on two types of context.
+
+The first of these, {name LContext}`LContext`, contains information that does
+not change throughout the type checking process, including built-in functions
+and types. TODO: talk about Factory and TypeFactory?
+
+{docstring Lambda.LContext}
+
+The second context includes two pieces of data that change throughout the type
+checking process: a map from free variables in expressions to types, and a list
+of type aliases including the name and definition of each alias.
+
+{docstring Lambda.TContext}
+
+Given these two pieces of context, the {name LExpr.HasType}`HasType` relation
+describes the valid type of each expression form.
 
 {docstring Lambda.LExpr.HasType}
 
-(TODO: talk about the type inference implementation?)
-
 ## Operational Semantics
+
+TODO: talk about how `Factory` fits into the operational semantics
 
 The semantics of the {name LExpr}`LExpr` type are specified in a standard way
 using the small-step inductive relation {name Lambda.Step}`Lambda.Step`.
@@ -168,9 +205,9 @@ The `Imperative` language is a standard core imperative calculus, parameterized
 by a type of expressions and divided into two pieces: commands and statements.
 Commands represent atomic operations that do not induce control flow (except
 possibly in the form of procedure calls that follow a stack discipline, though
-the current core set of commands does not include calls).  Statements are
+the current core set of commands does not include calls). Statements are
 parameterized by a command type and describe the control flow surrounding those
-commands. Statements exist in three forms, corresponding to the most common
+commands. Statements exist in two forms, corresponding to the most common
 alternative representations. These forms are:
 
 * Structured deterministic statements, each of which can be: a command, a
@@ -181,14 +218,14 @@ alternative representations. These forms are:
   sequence of two statements, an arbitrary choice between two statements, or an
   iteration of a statement an arbitrary number of times.
 
-* Unstructured control-flow graphs where each basic block consists of a sequence
-  of commands followed by a terminator command. A terminator command can be: a
-  conditional jump to one of two blocks, termination of execution, or a
-  non-deterministic jump to any one of an arbitrary number of successor blocks.
+A translation exists from structured deterministic statements into structured
+non-deterministic statements.
 
-Translations exist from structured deterministic statements into either
-structured non-deterministic statements or unstructured control-flow graphs.
-(TODO: point to these translations)
+We also expect to add unstructured control-flow graphs where each basic block
+consists of a sequence of commands followed by a terminator command. A
+terminator command can be: a conditional jump to one of two blocks, termination
+of execution, or a non-deterministic jump to any one of an arbitrary number of
+successor blocks.
 
 ## Command Syntax
 
@@ -225,7 +262,8 @@ a standard way.
 ## Structured Deterministic Statement Syntax
 
 Statements allow commands to be organized into standard control flow
-arrangements, including sequencing, alternation, and iteration.
+arrangements, including sequencing, alternation, and iteration. Sequencing
+statements occurs by grouping them into blocks.
 
 (TODO: say more about loop invariants, measures?)
 
@@ -262,14 +300,6 @@ relation.
 ## Structured Non-Deterministic Statement Operational Semantics
 
 {docstring Imperative.EvalNondetStmt}
-
-## Unstructured Control-Flow Graph Syntax
-
-TODO: fill in once it's merged
-
-## Unstructured Control-Flow Graph Semantics
-
-TODO: fill in once it's merged
 
 # Metadata
 
