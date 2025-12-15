c397cb5baf3ee6bc49ed7af08a9ecde0c0983f93
Remy Willems <rwillems@amazon.com>
2025-12-15 14:44:01 +0100

    Attempt at translating to Boogie

--- Strata/Languages/Laurel/LaurelToBoogieTranslator.lean
+++ Strata/Languages/Laurel/LaurelToBoogieTranslator.lean
@@ -16,14 +16,77 @@ namespace Laurel
 open Boogie (VCResult VCResults)
 open Strata
 
+open Boogie (intAddOp)
+open Lambda (LMonoTy LTy)
+
+/-
+Translate Laurel HighType to Boogie Type
+-/
+def translateType (ty : HighType) : LMonoTy :=
+  match ty with
+  | .TInt => LMonoTy.int
+  | .TBool => LMonoTy.bool
+  | .TVoid => LMonoTy.bool  -- Using bool as placeholder for void
+  | _ => LMonoTy.int  -- Default to int for other types
+
 /-
 Translate Laurel StmtExpr to Boogie Expression
 -/
 partial def translateExpr (expr : StmtExpr) : Boogie.Expression.Expr :=
   match expr with
-  | .LiteralBool true => .boolConst () true
-  | .LiteralBool false => .boolConst () false
-  | _ => .boolConst () true  -- TODO: handle other expressions
+  | .LiteralBool b => .const () (.boolConst b)
+  | .LiteralInt i => .const () (.intConst i)
+  | .Identifier name =>
+      let ident := Boogie.BoogieIdent.locl name
+      .fvar () ident (some LMonoTy.int)  -- Default to int type
+  | .PrimitiveOp .Add args =>
+      match args with
+      | [e1, e2] =>
+          let be1 := translateExpr e1
+          let be2 := translateExpr e2
+          .app () (.app () intAddOp be1) be2
+      | e1 :: e2 :: _ =>  -- More than 2 args
+          let be1 := translateExpr e1
+          let be2 := translateExpr e2
+          .app () (.app () intAddOp be1) be2
+      | [_] | [] => .const () (.intConst 0)  -- Error cases
+  | .PrimitiveOp .Eq args =>
+      match args with
+      | [e1, e2] =>
+          let be1 := translateExpr e1
+          let be2 := translateExpr e2
+          .eq () be1 be2
+      | e1 :: e2 :: _ =>  -- More than 2 args
+          let be1 := translateExpr e1
+          let be2 := translateExpr e2
+          .eq () be1 be2
+      | [_] | [] => .const () (.boolConst false)  -- Error cases
+  | .PrimitiveOp .Neq args =>
+      match args with
+      | [e1, e2] =>
+          let be1 := translateExpr e1
+          let be2 := translateExpr e2
+          -- Negate equality
+          .app () (.op () (Boogie.BoogieIdent.glob "Bool.Not") (some LMonoTy.bool)) (.eq () be1 be2)
+      | e1 :: e2 :: _ =>  -- More than 2 args
+          let be1 := translateExpr e1
+          let be2 := translateExpr e2
+          .app () (.op () (Boogie.BoogieIdent.glob "Bool.Not") (some LMonoTy.bool)) (.eq () be1 be2)
+      | [_] | [] => .const () (.boolConst false)  -- Error cases
+  | .IfThenElse cond thenBranch elseBranch =>
+      let bcond := translateExpr cond
+      let bthen := translateExpr thenBranch
+      let belse := match elseBranch with
+                  | some e => translateExpr e
+                  | none => .const () (.intConst 0)
+      .ite () bcond bthen belse
+  | .Assign _ value => translateExpr value  -- For expressions, just translate the value
+  | .StaticCall name args =>
+      -- Create function call as an op application
+      let ident := Boogie.BoogieIdent.glob name
+      let fnOp := .op () ident (some LMonoTy.int)  -- Assume int return type
+      args.foldl (fun acc arg => .app () acc (translateExpr arg)) fnOp
+  | _ => .const () (.intConst 0)  -- Default for unhandled cases
 
 /-
 Translate Laurel StmtExpr to Boogie Statements
@@ -31,24 +94,85 @@ Translate Laurel StmtExpr to Boogie Statements
 partial def translateStmt (stmt : StmtExpr) : List Boogie.Statement :=
   match stmt with
   | @StmtExpr.Assert cond md =>
-    let boogieExpr := translateExpr cond
-    [Boogie.Statement.assert "assert" boogieExpr md]
+      let boogieExpr := translateExpr cond
+      [Boogie.Statement.assert "assert" boogieExpr md]
   | @StmtExpr.Assume cond md =>
-    let boogieExpr := translateExpr cond
-    [Boogie.Statement.assume "assume" boogieExpr md]
+      let boogieExpr := translateExpr cond
+      [Boogie.Statement.assume "assume" boogieExpr md]
   | .Block stmts _ =>
-    stmts.flatMap translateStmt
-  | _ => []  -- TODO: handle other statements
+      stmts.flatMap translateStmt
+  | .LocalVariable name ty initializer =>
+      let boogieMonoType := translateType ty
+      let boogieType := LTy.forAll [] boogieMonoType
+      let ident := Boogie.BoogieIdent.locl name
+      match initializer with
+      | some initExpr =>
+          let boogieExpr := translateExpr initExpr
+          [Boogie.Statement.init ident boogieType boogieExpr]
+      | none =>
+          -- Initialize with default value
+          let defaultExpr := match ty with
+                            | .TInt => .const () (.intConst 0)
+                            | .TBool => .const () (.boolConst false)
+                            | _ => .const () (.intConst 0)
+          [Boogie.Statement.init ident boogieType defaultExpr]
+  | .Assign target value =>
+      match target with
+      | .Identifier name =>
+          let ident := Boogie.BoogieIdent.locl name
+          let boogieExpr := translateExpr value
+          [Boogie.Statement.set ident boogieExpr]
+      | _ => []  -- Can only assign to simple identifiers
+  | .IfThenElse cond thenBranch elseBranch =>
+      let bcond := translateExpr cond
+      let bthen := translateStmt thenBranch
+      let belse := match elseBranch with
+                  | some e => translateStmt e
+                  | none => []
+      -- Boogie doesn't have if-else statements directly, we need to use havoc + assume
+      -- For now, just translate branches and add conditional assumes
+      let thenStmts := (Boogie.Statement.assume "then" bcond) :: bthen
+      let elseStmts := match elseBranch with
+                      | some _ =>
+                          let notCond := .app () (.op () (Boogie.BoogieIdent.glob "Bool.Not") (some LMonoTy.bool)) bcond
+                          (Boogie.Statement.assume "else" notCond) :: belse
+                      | none => []
+      thenStmts ++ elseStmts
+  | .StaticCall name args =>
+      let boogieArgs := args.map translateExpr
+      [Boogie.Statement.call [] name boogieArgs]
+  | _ => []  -- Default for unhandled cases
+
+/-
+Translate Laurel Parameter to Boogie Signature entry
+-/
+def translateParameterToBoogie (param : Parameter) : (Boogie.BoogieIdent × LMonoTy) :=
+  let ident := Boogie.BoogieIdent.locl param.name
+  let ty := translateType param.type
+  (ident, ty)
 
 /-
 Translate Laurel Procedure to Boogie Procedure
 -/
 def translateProcedure (proc : Procedure) : Boogie.Procedure :=
+  -- Translate input parameters
+  let inputPairs := proc.inputs.map translateParameterToBoogie
+  let inputs := inputPairs
+
+  -- Translate output type
+  let outputs :=
+    match proc.output with
+    | .TVoid => []  -- No return value
+    | _ =>
+        let retTy := translateType proc.output
+        let retIdent := Boogie.BoogieIdent.locl "result"
+        [(retIdent, retTy)]
+
   let header : Boogie.Procedure.Header := {
     name := proc.name
     typeArgs := []
-    inputs := []
-    outputs := []
+    inputs := inputs
+    outputs := outputs
   }
   let spec : Boogie.Procedure.Spec := {
     modifies := []
