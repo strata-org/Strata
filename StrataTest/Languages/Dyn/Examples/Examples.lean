ff764191a23a3044c434e2bd9e9f961a0d00016c
Remy Willems <rwillems@amazon.com>
2025-12-15 12:08:49 +0100

    Moved examples

---
+++ StrataTest/Languages/Dyn/Examples/Examples.lean
@@ -0,0 +1,15 @@
+/-
+  Copyright Strata Contributors
+
+  SPDX-License-Identifier: Apache-2.0 OR MIT
+-/
+
+import StrataTest.Languages.Dyn.Examples.Trivial
+import StrataTest.Languages.Dyn.Examples.BasicTypes
+import StrataTest.Languages.Dyn.Examples.ListOperations
+import StrataTest.Languages.Dyn.Examples.ControlFlow
+import StrataTest.Languages.Dyn.Examples.Arithmetic
+import StrataTest.Languages.Dyn.Examples.StringOps
+import StrataTest.Languages.Dyn.Examples.TypeIntrospection
+import StrataTest.Languages.Dyn.Examples.HeapOps
+import StrataTest.Languages.Dyn.Examples.FunctionCalls
