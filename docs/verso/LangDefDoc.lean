ffced95cbefdc3ea93eb819ff38c9a82bb79e677
Aaron Tomb <aarotomb@amazon.com>
2025-12-17 16:13:37 -0800

    Move back to build failure on missing docstrings

--- docs/verso/LangDefDoc.lean
+++ docs/verso/LangDefDoc.lean
@@ -28,7 +28,7 @@ open Verso.Genre Manual
 open Verso.Genre.Manual.InlineLean
 
 set_option pp.rawOnError true
-set_option verso.docstring.allowMissing true
+set_option verso.docstring.allowMissing false
 
 #doc (Manual) "The Strata Language Definition" =>
 %%%
