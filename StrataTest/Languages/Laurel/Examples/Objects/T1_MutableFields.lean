086bf22eec6fd01f469f7d507d642851c5e86b45
Remy Willems <rwillems@amazon.com>
2026-01-14 18:18:44 +0100

    Draft work

--- StrataTest/Languages/Laurel/Examples/Objects/T1_MutableFields.lean
+++ StrataTest/Languages/Laurel/Examples/Objects/T1_MutableFields.lean
@@ -1,19 +1,31 @@
-/*
+/-
   Copyright Strata Contributors
 
   SPDX-License-Identifier: Apache-2.0 OR MIT
-*/
+-/
 
-composite Container {
-  var value: int // var indicates mutable field
-}
+import StrataTest.Util.TestDiagnostics
+import StrataTest.Languages.Laurel.TestExamples
 
-procedure foo(c: Container, d: Container): int
+open StrataTest.Util
+
+namespace Strata
+namespace Laurel
+
+def program := r"
+//composite Container {
+  // var value: int // var indicates mutable field
+//}
+
+procedure foo(c: Container, d: Container) returns int
   requires c != d
-{ 
-  var x = c.value;
-  d.value = d.value + 1;
+{
+  var x := c.value;
+  d.value := d.value + 1;
   assert x == c.value; // pass
+
+  var e := d;
+  assert e.value == d.value;
 }
 
 procedure caller(c: Container, d: Container) {
@@ -21,10 +33,13 @@ procedure caller(c: Container, d: Container) {
 }
 
 procedure impureContract(c: Container)
-  ensures foo(c, c) 
+  ensures foo(c, c)
 //           ^ error: a procedure that modifies the heap may not be called in pure context.
+"
 
-/*
+#eval testInputWithOffset "MutableFields" program 14 processLaurelFile
+
+/-
 Translation towards SMT:
 
 type Composite;
@@ -64,4 +79,4 @@ proof caller {
   (x, heap) = foo(heap, c, d);
   heap_out = heap;
 }
-*/
+-/
