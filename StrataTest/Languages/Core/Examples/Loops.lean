c6fa27831d58eca3075d3b19310b7fac4fdf4814
Lydia Kondylidou <33327056+kondylidou@users.noreply.github.com>
2026-02-04 16:49:18 +0100

    fixing the StrataTests

--- StrataTest/Languages/Core/Examples/Loops.lean
+++ StrataTest/Languages/Core/Examples/Loops.lean
@@ -23,9 +23,9 @@ spec {
   i := 0;
   s := 0;
   while (i < n)
-    invariant 0 <= i &&
-              i <= n &&
-              s == (i * (i + 1)) div 2;
+    invariant 0 <= i
+    invariant i <= n
+    invariant s == (i * (i + 1)) div 2
   {
     i := (i + 1);
     s := (s + i);
