64fbd0be9c22de0e426e149dbd2a573024d66083
Joe Hendrix <joehx@amazon.com>
2026-02-03 16:47:16 -0800

    Update to 4.27.0

--- Strata/DDM/Integration/Java/Gen.lean
+++ Strata/DDM/Integration/Java/Gen.lean
@@ -349,7 +349,9 @@ def generateDialect (d : Dialect) (package : String) : Except String GeneratedFi
     | _ => none
 
   -- All interface names for Node permits clause
-  let allInterfaceNames := (sealedInterfaces ++ stubInterfaces).map (·.1.dropRight 5)
+  let allInterfaceNames := 
+        sealedInterfaces ++ stubInterfaces 
+        |>.map (·.1.dropEnd 5 |>.toString)
 
   return {
     sourceRange := generateSourceRange package
