2bc1337441622592903f802d02480a6b231fefda
Remy Willems <rwillems@amazon.com>
2026-02-12 12:33:28 +0000

    add copyright header

--- Tools/fix_guard_msgs.py
+++ Tools/fix_guard_msgs.py
@@ -1,3 +1,6 @@
+# Copyright Strata Contributors
+#
+#  SPDX-License-Identifier: Apache-2.0 OR MIT
 #!/usr/bin/env python3
 """
 Parse `lake test` output to extract #guard_msgs diffs and apply fixes.
