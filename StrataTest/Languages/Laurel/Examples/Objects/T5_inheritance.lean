463f98757ba5f48073c4978e580fd8de806edbf1
Remy Willems <rwillems@amazon.com>
2026-02-18 16:19:04 +0000

    Add test for inheritance

---
+++ StrataTest/Languages/Laurel/Examples/Objects/T5_inheritance.lean
@@ -0,0 +1,43 @@
+/-
+  Copyright Strata Contributors
+
+  SPDX-License-Identifier: Apache-2.0 OR MIT
+-/
+
+import StrataTest.Util.TestDiagnostics
+import StrataTest.Languages.Laurel.TestExamples
+
+open StrataTest.Util
+
+namespace Strata
+namespace Laurel
+
+def program := r"
+composite Base {
+  var xValue: int
+}
+
+composite Extender extends Base {
+  var yValue: int
+}
+
+procedure typeCheckingAndCasting() {
+  var a: Base := new Base;
+  assert a is Base;
+  assert !(a is Extender);
+  var b: Extender := new
+  assert b is Base; Extender;
+  assert b is Extender;
+
+  var c: Base := b;
+  var d: Extender := c as Extender;
+  var e: Extender := a as Extender;
+//                   ^^^^^^^^^^^^^ error: assertion does not hold
+
+  b#xValue := 1;
+  b#yValue := 2;
+}
+"
+
+#guard_msgs(drop info, error) in
+#eval testInputWithOffset "Inheritance" program 14 processLaurelFile
