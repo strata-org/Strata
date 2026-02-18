3da6bed79ba44c098def4c5976645d489a083310
Remy Willems <rwillems@amazon.com>
2026-02-18 16:18:54 +0000

    Copy infinite loop parsing check from 385

--- Strata/DDM/Elab.lean
+++ Strata/DDM/Elab.lean
@@ -71,6 +71,11 @@ private partial def runCommand (leanEnv : Lean.Environment) (commands : Array Op
     return commands
   let (some tree, true) ← runChecked <| elabCommand leanEnv
     | return commands
+  -- Prevent infinite loop if parser makes no progressCollapse commentComment on line R74keyboardDrummer commented on Feb 16, 2026 keyboardDrummeron Feb 16, 2026ContributorMore actionsThis is great!!ReactWrite a replyResolve comment
+  let newPos := (←get).pos
+  if newPos <= iniPos then
+    logError { start := iniPos, stop := iniPos } "Syntax error: unrecognized syntax or unexpected token"
+    return commands
   let cmd := tree.info.asOp!.op
   let dialects := (← read).loader.dialects
   modify fun s => { s with
