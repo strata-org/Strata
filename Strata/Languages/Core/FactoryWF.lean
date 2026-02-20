34ff7c3ae07380b6e92b1aa1391aabf781f8f158
Juneyoung Lee <lebjuney@amazon.com>
2026-02-19 21:35:43 -0600

    Reduce maxRecDepth of Factory_wf from 32768 to 1024

--- Strata/Languages/Core/FactoryWF.lean
+++ Strata/Languages/Core/FactoryWF.lean
@@ -17,7 +17,13 @@ import Strata.DL.Lambda.IntBoolFactory
 namespace Core
 open Lambda
 
-set_option maxRecDepth 32768 in
+theorem array_list_happend_toArray:
+  ∀ {α:Type} (a:Array α) (b:List α), a ++ b = (a.toList ++ b).toArray
+  := by
+  unfold HAppend.hAppend Array.instHAppendList Array.appendList
+  grind
+
+set_option maxRecDepth 1024 in
 set_option maxHeartbeats 4000000 in
 /--
 Wellformedness of Factory
@@ -25,42 +31,35 @@ Wellformedness of Factory
 theorem Factory_wf :
     FactoryWF Factory := by
   unfold Factory
+  simp only [array_list_happend_toArray, List.cons_append, List.nil_append]
   apply FactoryWF.mk
   · decide -- FactoryWF.name_nodup
-  · unfold HAppend.hAppend Array.instHAppendList
-    simp only []
-    unfold Array.appendList
-    simp only [List.foldl, Array.push, List.concat]
-    intros lf
-    rw [← Array.mem_toList_iff]
-    simp only []
-    intros Hmem
-    repeat (
-      rcases Hmem with _ | ⟨ a', Hmem ⟩
-      · rw [LFuncWF]
-        apply Strata.DL.Util.FuncWF.mk
-        · decide -- LFuncWF.arg_nodup
-        · decide -- LFuncWF.body_freevars
-        · -- LFuncWF.concreteEval_argmatch
-          simp (config := { ground := true })
-          try (
-            try unfold unOpCeval
-            try unfold binOpCeval
-            try unfold cevalIntDiv
-            try unfold cevalIntMod
-            try unfold cevalIntDivT
-            try unfold cevalIntModT
-            try unfold bvUnaryOp
-            try unfold bvBinaryOp
-            try unfold bvShiftOp
-            try unfold bvBinaryPred
-            intros lf md args res
-            repeat (rcases args with _ | ⟨ args0, args ⟩ <;> try grind))
-        · decide -- LFuncWF.body_or_concreteEval
-        · decide -- LFuncWF.typeArgs_nodup
-        · decide -- LFuncWF.inputs_typevars_in_typeArgs
-        · decide -- LFuncWF.output_typevars_in_typeArgs
+  · intros f Hmem
+    iterate 176 (any_goals (rcases Hmem with _ | ⟨ a', Hmem ⟩ <;> try contradiction))
+    all_goals (
+      rw [LFuncWF]
+      apply Strata.DL.Util.FuncWF.mk
+      · decide -- LFuncWF.arg_nodup
+      · decide -- LFuncWF.body_freevars
+      · -- LFuncWF.concreteEval_argmatch
+        simp (config := { ground := true })
+        try (
+          try unfold unOpCeval
+          try unfold binOpCeval
+          try unfold cevalIntDiv
+          try unfold cevalIntMod
+          try unfold cevalIntDivT
+          try unfold cevalIntModT
+          try unfold bvUnaryOp
+          try unfold bvBinaryOp
+          try unfold bvShiftOp
+          try unfold bvBinaryPred
+          intros lf md args res
+          repeat (rcases args with _ | ⟨ args0, args ⟩ <;> try grind))
+      · decide -- LFuncWF.body_or_concreteEval
+      · decide -- LFuncWF.typeArgs_nodup
+      · decide -- LFuncWF.inputs_typevars_in_typeArgs
+      · decide -- LFuncWF.output_typevars_in_typeArgs
     )
-    contradiction
 
 end Core
