c9ed9fa2d2bf3b34a6ac588577d6a3c5ba1e2a48
Remy Willems <rwillems@amazon.com>
2026-02-25 14:19:54 +0000

    Add more test cases

--- StrataTest/Languages/Laurel/Examples/Fundamentals/T8_Postconditions.lean
+++ StrataTest/Languages/Laurel/Examples/Fundamentals/T8_Postconditions.lean
@@ -15,16 +15,38 @@ namespace Strata.Laurel
 def program := r"
 procedure opaqueBody(x: int) returns (r: int)
 // the presence of the ensures make the body opaque. we can consider more explicit syntax.
-  ensures (r >= 0)
+  ensures r > 0
 {
   if (x > 0) { r := x; }
   else { r := 1; }
+}
+
+procedure callerOfOpaqueProcedure() {
+  var x: int := opaqueBody(3);
+  assert x > 0;
+  assert x == 3;
+//^^^^^^^^^^^^^^ error: assertion does not hold
 }
 
 procedure invalidPostcondition(x: int)
     ensures false
 //          ^^^^^ error: assertion does not hold
 {
+}
+
+function opaqueFunction(x: int) returns (r: int)
+  requires x > 0
+  ensures r > 0
+{
+  x
+}
+
+procedure callerOfOpaqueFunction() {
+  var x: int := opaqueFunction(3);
+  assert x > 0;
+// The following assert should fail but it does not,
+// because Core does not support opaque functions
+//  assert x == 3;
 }
 "
 
