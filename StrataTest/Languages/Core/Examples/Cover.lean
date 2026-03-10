2e8832281e46953b471e255de66d0b2be6406559
Olivier Bouissou <obouisso@amazon.com>
2026-03-10 17:21:01 +0000

    Update order in expected messages

--- StrataTest/Languages/Core/Examples/Cover.lean
+++ StrataTest/Languages/Core/Examples/Cover.lean
@@ -33,18 +33,6 @@ procedure Test() returns ()
 
 /--
 info:
-Obligation: unreachable_cover1
-Property: cover
-Result: ❌ fail
-
-Obligation: unreachable_cover2
-Property: cover
-Result: ❌ fail
-
-Obligation: unreachable_assert
-Property: assert
-Result: ✅ pass
-
 Obligation: reachable_cover
 Property: cover
 Result: ✅ pass
@@ -54,6 +42,18 @@ Property: cover
 Result: ❌ fail
 
 Obligation: reachable_assert
+Property: assert
+Result: ✅ pass
+
+Obligation: unreachable_cover1
+Property: cover
+Result: ❌ fail
+
+Obligation: unreachable_cover2
+Property: cover
+Result: ❌ fail
+
+Obligation: unreachable_assert
 Property: assert
 Result: ✅ pass
 -/
