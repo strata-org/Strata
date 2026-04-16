f410650d27b582529ff8da3913efd60e793523b2
Remy Willems <rwillems@amazon.com>
2026-04-16 16:45:53 +0200

    Test update

--- StrataTest/Languages/Laurel/Examples/Objects/T2_ModifiesClauses.lean
+++ StrataTest/Languages/Laurel/Examples/Objects/T2_ModifiesClauses.lean
@@ -88,7 +88,7 @@ procedure modifyContainerWithoutPermission3(c: Container, d: Container)
   opaque
   modifies d
 {
-    var i: int := modifyContainerTransparant(c)
+    var i: int := modifyContainerOpaque(c)
 };
 
 procedure multipleModifiesClauses(c: Container, d: Container, e: Container)
