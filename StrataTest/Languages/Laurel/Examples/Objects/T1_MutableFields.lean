128ade3fad65051fff5f9f9f32133c124903647f
Remy Willems <rwillems@amazon.com>
2026-04-16 15:18:41 +0200

    Fix test

--- StrataTest/Languages/Laurel/Examples/Objects/T1_MutableFields.lean
+++ StrataTest/Languages/Laurel/Examples/Objects/T1_MutableFields.lean
@@ -67,17 +67,21 @@ procedure updatesAndAliasing()
   assert dAlias#intValue == d#intValue
 };
 
-procedure subsequentHeapMutations(c: Container)
+procedure subsequentHeapMutations()
   opaque
 {
+  var c: Container := new Container;
   // The additional parenthesis on the next line are needed to let the parser succeed. Joe, any idea why this is needed?
   var sum: int := ((c#intValue := 1) + c#intValue) + (c#intValue := 2);
   assert sum == 4
 };
 
-procedure implicitEquality(c: Container, d: Container)
+procedure implicitEquality()
   opaque
 {
+  var c: Container := new Container;
+  var d: Container := new Container;
+
   c#intValue := 1;
   d#intValue := 2;
   if c#intValue == d#intValue then {
@@ -98,9 +102,12 @@ composite SameFieldName {
   var intValue: bool
 }
 
-procedure sameFieldNameDifferentType(a: Container, b: SameFieldName)
+procedure sameFieldNameDifferentType()
   opaque
 {
+  var a: Container := new Container;
+  var b: SameFieldName := new SameFieldName;
+
   a#intValue := 1;
   b#intValue := true;
 
