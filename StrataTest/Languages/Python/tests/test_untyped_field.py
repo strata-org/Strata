877a33a8e63ccc2774226dfd4a742e09d78ab144
Olivier Bouissou <obouisso@amazon.com>
2026-04-27 21:50:21 +0000

    add negative test case

--- StrataTest/Languages/Python/tests/test_untyped_field.py
+++ StrataTest/Languages/Python/tests/test_untyped_field.py
@@ -3,8 +3,13 @@ class Bag:
     def __init__(self, item):
         self.item = item
 
-def test_untyped_field():
+def test_untyped_field_ok():
     b = Bag(42)
     assert b.item == 42, "untyped field"
 
-test_untyped_field()
+def test_untyped_field_ko():
+    b = Bag(44)
+    assert b.item == 42, "untyped field must fail"
+
+test_untyped_field_ok()
+test_untyped_field_ko()
