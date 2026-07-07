06bab7f96c76574db67a766970a353cd368699ca
Remy Willems <rwillems@amazon.com>
2026-07-07 12:40:04 +0000

    More changes

--- docs/verso/LaurelDesignGuide.lean
+++ docs/verso/LaurelDesignGuide.lean
@@ -22,8 +22,6 @@ open Verso.Genre Manual
 -- environment as Verso
 open Verso.Genre.Manual.InlineLean
 
-
-
 #doc (Manual) "The Laurel Language Design Guide" =>
 %%%
 shortTitle := "Laurel Design"
@@ -44,13 +42,22 @@ Goals:
   2. data-flow analysis
   3. symbolic execution (aka verification), both bounded and unbounded
 2. Reduce code duplication in the analysis of popular languages by being a target for compilation from those languages, and including features common to them. Note that we expect source languages to reduce their existing compilers when possible, so language features that can be compiled away don't need to be considered.
-3. Minimize the amount of user code needed to enable verification.
+3. Have a great user experience
 4. Enable modular verification
-5. Use complete analysis algorithms to reduce the required proof effort.
+5. Minimize the amount of user code needed to enable verification.
 6. Enable finding proofs through an automated search.
-7. Code used to enable verification may not affect execution behavior.
+7. Use complete analysis algorithms to reduce the required proof effort.
+8. Code used to enable verification may not affect execution behavior.
 
-# Reduce duplication between source languages
+# (1.3) Enable Verification
+To achieve goal 1.3, enable proving properties through verification, Laurel has the following features.
+- Assertions
+- Quantifiers
+- Old/allocated/fresh
+- Decreases clauses
+- Assumptions (more about gradual verification)
+
+# (2) Reduce duplication between source languages
 To achieve goal (2), reduce code duplication in the analysis of popular languages, Laurel contains many features shared between several languages. The following table shows which features are shared with which input languages.
 
 TODO, add a table with Laurel features as rows, and a list of languages in columns (Java, Kotlin, C#, JavaScript, Python, GoLang). Split up language feature whenever relevant for the columns.
@@ -60,43 +67,46 @@ These features should be in rows but marked as WIP:
 - Procedures types and procedures as values
 - Parametric polymorphism
 
-# Enable Verification
-To achieve goal 1.3, enable proving properties through verification, Laurel has the following features.
-- Assertions
-- Quantifiers
-- Old/allocated/fresh
-- Decreases clauses
-- Loop invariants (more about automated proof)
-- Assumptions (more about gradual verification)
+# (3) Great user experience
 
-# Modular Verification
+## Parameter lists
+Parameter lists. In Laurel, input and output parameters are defined in a separate list. Inout parameters are defined by repeating the parameter name in both lists. In Core, there is a single parameter list where each parameter defines its kind (in/out/inout).
+
+At the call-site, Laurel requires calls with multiple out parameters to occur inside an assignment, like this:
+`assign x, y := multiOutCall(a, b)`
+Core uses the argument list to assign the output parameters, like this:
+`multiOutCall(a, b, out x, out y)`
+
+In Laurel, an inout parameter only influences the callee's code, since it means there is a single variable that is used as input and output. On the calling side however, there is no concept of inout parameters. This is different from Core, where inout variables affect the calling side. Example of an inout being called in Core, `hasInout(inout x)`.
+
+## Assignments to fresh and existing declarations
+In Laurel, assignments can have multiple targets. Each target can be either an existing variable or a local declaration. Example:
+```
+var x: int;
+var z: int;
+assign x, var y: int, z := hasThreeOutputs()
+```
+In Core, when calling a procedure with multiple outputs, each output parameter must be assigned to an existing local variable. Example:
+```
+var x: int;
+var y: int;
+var z: int;
+hasThreeOutputs(out x, out y, out z);
+```
+
+# (4) Modular Verification
 To achieve goal (4), Laurel has the following features related to modular verification.
 
 ## Preconditions
 Preconditions enable proving the assertions in a procedure's body without having to consider the callers. This way, each assertion only needs to be proven once, instead of once for each transitive call-site.
 
-## Encapsulation
-Postconditions enable encapsulating the behavior of a procedure through a simpler condition. This simplifies reasoning at the call-site.
+## Opaque procedures
+Laurel allows a procedure to be marked as opaque, which means that callers won't get access to the body of the procedure. Laurel only allows postconditions for opaque procedures. This restriction is because a postcondition can only contain information that can also be inferred from the body, and redundant information is bad for verification performance.
 
-Since procedures can mutate references as well, once we add encapsulation through postconditions, we also need modifies clauses to enable encapsulating reference modifying procedures.
+Since modifies clauses are a type of postcondition, they are also only allowed on opaque procedures.
 
-Reads clauses are useful to improve verification performance. The facts they prove work well together with the facts provided by modifies clauses, making it easier to prove which procedure values have remained unchanged after objects were modified.
-
-# Reduce verification through complete analysis
-To achieve goal 5, Laurel has the following features.
-
-## Constrained types
-Constrained types propagating facts through the type system.
-
-TODO, add example using polymorphic types
-
-## Implicit conversions
-
-To be designed..
-
-
-# Minimize Verification Code
-To achieve goal (3), minimize the amount of user code needed to enable verification, Laurel has the following features:
+# (5) Minimize Verification Code
+To achieve goal (5), minimize the amount of user code needed to enable verification, Laurel has the following features:
 
 ## Transparent procedures
 Laurel procedures are transparent by default, meaning that a call can use the body of the callee to prove facts about the result of the call. Laurel will allow any procedure to be transparent, although currently there are some restrictions. In particular, Laurel will allow procedures that contains loops or that modify the heap, to be transparent as well.
@@ -123,31 +133,26 @@ A second reason for not allowing any heap modification inside contracts is that 
 
 TODO, fill in
 
-# Naming choices
+# (6) Enable finding proofs through an automated search.
 
-# Differences between Laurel and Core
+Loop invariants
 
-## Parameter lists
-Parameter lists. In Laurel, input and output parameters are defined in a separate list. Inout parameters are defined by repeating the parameter name in both lists. In Core, there is a single parameter list where each parameter defines its kind (in/out/inout).
+Reads clauses are useful to improve verification performance. The facts they prove work well together with the facts provided by modifies clauses, making it easier to prove which procedure values have remained unchanged after objects were modified.
 
-At the call-site, Laurel requires calls with multiple out parameters to occur inside an assignment, like this:
-`assign x, y := multiOutCall(a, b)`
-Core uses the argument list to assign the output parameters, like this:
-`multiOutCall(a, b, out x, out y)`
+# (7) Reduce verification through complete analysis
+To achieve goal 7, Laurel has the following features.
 
-In Laurel, an inout parameter only influences the callee's code, since it means there is a single variable that is used as input and output. On the calling side however, there is no concept of inout parameters. This is different from Core, where inout variables affect the calling side. Example of an inout being called in Core, `hasInout(inout x)`.
+## Constrained types
+Constrained types propagating facts through the type system.
 
-## Assignments to fresh and existing declarations
-In Laurel, assignments can have multiple targets. Each target can be either an existing variable or a local declaration. Example:
-```
-var x: int;
-var z: int;
-assign x, var y: int, z := hasThreeOutputs()
-```
-In Core, when calling a procedure with multiple outputs, each output parameter must be assigned to an existing local variable. Example:
-```
-var x: int;
-var y: int;
-var z: int;
-hasThreeOutputs(out x, out y, out z);
-```
+TODO, add example using polymorphic types
+
+## Implicit conversions
+
+To be designed..
+
+# (8) Verification code may not affect execution
+
+TODO, Rules for contracts:
+- Contract code may not modify variables defined outside the contract scope.
+- Contract code has an empty modifies clause. Contract code operates on a copy of the heap.
